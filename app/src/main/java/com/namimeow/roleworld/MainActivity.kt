c1cfbd6604ccb813b297a35ad4b17311d989abc1
NAmi-meow <nami.github.dev@gmail.com>
2026-06-12 13:05:01 +0500

    Initial Commit
    Simple started app

---
+++ app/src/main/java/com/namimeow/roleworld/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.namimeow.roleworld
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(saveInstanceState: Bundle?) {
+        super.onCreate(saveInstanceState)
+    }
+}
